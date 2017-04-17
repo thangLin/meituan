fa5ba9929db833f6124938cedc274cf01c36b841
unknown <>
2017-04-17 19:47:54 +0800

    eee

--- src/js/wdMT/clickRestaurant.js
+++ src/js/wdMT/clickRestaurant.js
@@ -3,6 +3,7 @@ function clickRestaurant(data){
        var index =  $(this).index();
        var oData = JSON.stringify(data[index]);
        sessionStorage.shopInfo = oData;
+       console.log(window.location.href)
        window.location.href = '../../meituan/second.html';
        //实现不用a标签就能跳转页面
     })
